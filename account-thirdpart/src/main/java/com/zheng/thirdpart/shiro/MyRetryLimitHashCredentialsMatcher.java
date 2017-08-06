7b7ccda38bdb182d253d2a2f649fadf8765d92f1
zl13637992082 <736732419@qq.com>
2017-08-06 23:37:04 +0800

    修改注释

--- account-thirdpart/src/main/java/com/zheng/thirdpart/shiro/MyRetryLimitHashCredentialsMatcher.java
+++ account-thirdpart/src/main/java/com/zheng/thirdpart/shiro/MyRetryLimitHashCredentialsMatcher.java
@@ -26,18 +26,18 @@ public class MyRetryLimitHashCredentialsMatcher extends HashedCredentialsMatcher
     public boolean doCredentialsMatch(AuthenticationToken token, AuthenticationInfo info) {
         String username = token.getPrincipal().toString();
         AtomicInteger count = cache.get(username);
-        if(!Optional.ofNullable(count).isPresent()) { // 初次登录，创建一个用户缓存
+        if(!Optional.ofNullable(count).isPresent()) { // 初次登录,创建一个用户缓存
             count = new AtomicInteger(0);
             cache.put(username, count);
         }
         
-        if(count.getAndIncrement() > 5) { // 超过5次就暂时禁用用户账号，5分钟后再登录
-            throw new ExcessiveAttemptsException("您输入密码错误次数太频繁，系统将暂时禁用当前用户，您可以5分钟再试！");
+        if(count.getAndIncrement() > 5) { // 超过5次就暂时禁用用户账号,5分钟后再登录
+            throw new ExcessiveAttemptsException("您输入密码错误次数太频繁,系统将暂时禁用当前用户,您可以5分钟再试!");
         }
         
         // 进行登录认证
         boolean isMatch = super.doCredentialsMatch(token, info);
-        if(isMatch) { //用户登录成功，需要清空缓存
+        if(isMatch) { //用户登录成功,需要清空缓存
             cache.remove(username);
         }
         
